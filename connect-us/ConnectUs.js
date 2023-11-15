a05b4c4ce7b55b206d8e51e6d90b208108bff232
mattstraczek <mstraczek10@gmail.com>
2023-11-15 16:42:27 -0600

    Add Log screen + backend

--- connect-us/ConnectUs.js
+++ connect-us/ConnectUs.js
@@ -1,4 +1,3 @@
-
 import { isLoaded, useFonts } from "expo-font";
 import React from "react";
 import { useState, useEffect } from "react";
@@ -11,51 +10,56 @@ import { onAuthStateChanged } from "firebase/auth";
 
 import LoadingScreen from "./screens/LoadingScreen";
 import HomeScreen from "./screens/HomeScreen";
-import DiaryScreen from "./screens/DiaryScreen"
+import DiaryScreen from "./screens/DiaryScreen";
 import LoginScreen from "./screens/LoginScreen";
 import LocationScreen from "./screens/LocationScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ForgotPassScreen from "./screens/ForgotPassScreen";
 import TimeCapsuleScreen from "./screens/TimeCapsuleScreen";
+import AddLogScreen from "./screens/AddLogScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const Tabs = () => {
+const Tabs = ({ user }) => {
   return (
-      <Tab.Navigator
-          screenOptions={{
-            headerShown: false,
-            tabBarActiveTintColor: "#e91d63",
-            tabBarLabelStyle: { fontSize: 11, color: "#e91d63" },
-            tabBarStyle: {
-              position: "absolute",
-              backgroundColor: "#666666",
-              borderTopWidth: 0,
-              color: "#e91d63",
-
-            },
-          }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Diary" component={DiaryScreen} />
-        <Tab.Screen name="Location" component={LocationScreen} />
-
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#e91d63",
+        tabBarLabelStyle: { fontSize: 11, color: "#e91d63" },
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "#666666",
+          borderTopWidth: 0,
+          color: "#e91d63",
+        },
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Diary" component={DiaryScreen} />
+      <Tab.Screen name="Location" component={LocationScreen} />
+      <Tab.Screen
+        name="AddLog"
+        component={AddLogScreen}
+        initialParams={{ uid: user?.uid }}
+      />
+    </Tab.Navigator>
   );
 };
 
-
 export default function ConnectUs() {
-
   const [isLoading, setIsLoading] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user && auth.currentUser.emailVerified) {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser && auth.currentUser.emailVerified) {
         console.log("Logged in");
         setLoggedIn(true);
+        setUser(currentUser);
+        // console.log(currentUser)
       } else {
         setLoggedIn(false);
       }
@@ -71,17 +75,15 @@ export default function ConnectUs() {
 
   if (!loggedIn) {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-          <Stack.Screen name="ForgotPass" component={ForgotPassScreen} />
-        </Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ForgotPass" component={ForgotPassScreen} />
+      </Stack.Navigator>
     );
   }
 
-  return (
-      <Tabs/>
-  );
+  return <Tabs user={user} />;
 }
 
 export { Tabs };
